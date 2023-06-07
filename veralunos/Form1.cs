807dfec305366c8985e931aed124a67ab37fdcb1
i5 4670K <i5 4670K@DESKTOP-NDA2IO5>
2023-06-07 10:26:38 -0300

    Adicionar arquivos de projeto.

---
+++ veralunos/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veralunos
+{
+    public partial class veralunos : Form
+    {
+        public veralunos()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPesquisaInscrição_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPesquisaInscrição.Text !="")
+            {
+                label1.Visible= false;
+                Image image = image.FromFile("C:/Liberay Management System/search1.gif");
+                pictureBox1.Image = image;
+            }
+            else
+            {
+                label1.Visible = true;
+                Image image = image.FromFile("C:/Liberay Management System/search.gif");
+                pictureBox1.Image = image;
+            }
+        }
+    }
+}
